176a337be9a6712d7da7673c0cf42bac7fe7dbdc
loghmanb <loghmanb@gmail.com>
2019-10-18 23:24:23 +0330

    revise ticket listing

--- tickets/admin.py
+++ tickets/admin.py
@@ -4,6 +4,7 @@ from .models import Ticket
 
 
 class TicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subject', 'assigned_to', 'submitter', 'priority', 'starred')
     fields = ['subject', 'description', 'assigned_to', 'submitter', 'priority', 'starred', 'create_date', 'create_user', 'write_date', 'write_user']
 
 

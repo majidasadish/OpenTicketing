95f611afc55ccf71c7bc6dd1165cf7e5f8957dd9
loghmanb <loghmanb@gmail.com>
2019-10-19 09:25:02 +0330

    revise admin page for ticket and comment

--- tickets/admin.py
+++ tickets/admin.py
@@ -5,13 +5,21 @@ from .models import Ticket, Comment
 
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('id', 'subject', 'assigned_to', 'submitter', 'priority', 'starred')
-    #fields = ['subject', 'description', 'assigned_to', 'submitter', 'priority', 'starred', 'create_date', 'create_user', 'write_date', 'write_user']
+    list_display_links = ('id', 'subject')
+    list_filter = ('assigned_to', 'starred')
+    list_editable = ('assigned_to', 'priority', 'starred')
+    search_fields = ('id', 'subject', 'description', 'assigned_to', 'submitter',)
+    fields = ('subject', 'description', 'assigned_to', 'submitter', 'priority', 'starred', 'create_date', 'create_user', 'write_date', 'write_user')
+    list_per_page = 20
 
 admin.site.register(Ticket, TicketAdmin)
 
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'note', 'ticket')
+    search_fields = ('id', 'note', 'ticket')
+    list_display_links = ('id', 'note')
+    list_per_page = 20
 
 admin.site.register(Comment, CommentAdmin)
 
